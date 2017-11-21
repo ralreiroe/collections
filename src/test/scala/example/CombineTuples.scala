0e039df49e47b4c01678087ddec4430848027d30
Ralf Oenning <ralf@invicto.net>
2017-11-21 22:16:28 +0000

    curr: (Int, Int) => (left,right)

--- src/test/scala/example/CombineTuples.scala
+++ src/test/scala/example/CombineTuples.scala
@@ -7,10 +7,10 @@ class CombineTuples extends Spec {
     val s1: Set[(Int, Int)] = Set((1, 2), (1, 3), (2, 4), (2, 5))
 
     def fold1(s: Set[(Int, Int)]): Set[(Int, Int)] = s.foldLeft(Map.empty[Int, Int]) {
-      case (acc, curr: (Int, Int)) => {
-        acc.get(curr._1) match {
-          case None => acc + (curr._1 -> curr._2)
-          case Some(r) => acc + (curr._1 -> (acc.get(curr._1).get + curr._2))
+      case (acc, (left,right)) => {
+        acc.get(left) match {
+          case None => acc + (left -> right)
+          case Some(r) => acc + (left -> (acc.get(left).get + right))
         }
       }
     }.toSet

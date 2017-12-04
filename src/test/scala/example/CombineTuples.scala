a8921cffc949d5f0872a3df3b9ec9334f4403738
Ralf Oenning <ralf@invicto.net>
2017-12-04 11:37:50 +0000

    combine  tuples

--- src/test/scala/example/CombineTuples.scala
+++ src/test/scala/example/CombineTuples.scala
@@ -2,7 +2,7 @@ package example
 
 class CombineTuples extends Spec {
 
-  "combine tuples using a map" in {
+  "combine tuples using a map explicitely" in {
 
     val s1: Set[(Int, Int)] = Set((1, 2), (1, 3), (2, 4), (2, 5))
 
@@ -18,4 +18,22 @@ class CombineTuples extends Spec {
     fold1(s1) shouldBe Set((1,5), (2,9))
 
   }
+
+  "combine tuples using a map implicitely" in {
+
+    val s1: Set[(Int, Int)] = Set((1, 2), (1, 3), (2, 4), (2, 5))
+
+    def fold1(s: Set[(Int, Int)]): Set[(Int, Int)] = {
+
+      val f: ((Int,Int)) => Int = { case ((a,b)) => a}
+      s.groupBy(f).map {
+        case (int, setofintint) => (int, setofintint.map(_._2).reduce(_+_))
+      }.toSet
+
+    }
+
+    fold1(s1) shouldBe Set((1,5), (2,9))
+
+  }
+
 }

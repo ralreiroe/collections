7e86eef8140de669526644d6ae7b8a56243652b0
Ralf Oenning <ralf@invicto.net>
2018-02-10 23:08:42 +0000

    preserving order

--- src/test/scala/example/Dedup.scala
+++ src/test/scala/example/Dedup.scala
@@ -4,10 +4,25 @@ class Dedup extends Spec {
 
   "groupBy f:A->B, el becomes key, f(el) is added to value" in {
 
-    Seq(1,1,3,4).groupBy(identity) shouldBe Map(4 -> List(4), 1 -> List(1, 1), 3 -> List(3))
-    println(Seq(1,1,3,4).groupBy(identity).keys)
+    Seq(3,1,4,1).groupBy(identity) shouldBe Map(4 -> List(4), 1 -> List(1, 1), 3 -> List(3))
+    Seq(1,1,3,4).groupBy(identity).keys shouldBe Set(4, 1, 3)
+  }
 
+  "dedup with preserving order" in {
 
+    def dedupPreservingOrder(ints: Seq[Int]): Seq[Int] = {
+      val s = scala.collection.mutable.HashSet.empty[Int]
+      ints.foldLeft(Seq.empty[Int]) {
+        case (acc, curr) => {
+          if (!s.apply(curr)) {
+            s.+=(curr)
+            curr +: acc
+          } else acc
+        }
+      }.reverse
+    }
+
+    dedupPreservingOrder(Seq(8, 8, 3, 1, 4, 1)) shouldBe Seq(8, 3,1,4)
   }
 
 }

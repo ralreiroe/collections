c0f64cb8e5ab46387447f0cd907ac6c5eaa17540
Ralf Oenning <ralf@invicto.net>
2019-06-30 13:10:49 +0100

    fix + iterative

--- src/test/scala/example/TailrecReverse.scala
+++ src/test/scala/example/TailrecReverse.scala
@@ -15,20 +15,29 @@ class TailrecReverse extends Spec {
     @tailrec
     def reversetr[A](resultsofar: List[A], remainder: List[A]): List[A] = {
       remainder match {
-        case h :: t => reversetr(resultsofar ::: List(h), t)    //(*')
+        case h :: t => reversetr(h :: resultsofar, t)           //(*')
         case Nil => resultsofar                                 //(**)
       }
     }
 
-    println(reverse(List(1, 2, 2, 2, 3)))
-    println(reversetr(List(), List(1, 2, 2, 2, 3)))
+    reverse(List(1, 2, 2, 2, 3)) shouldBe List(3, 2, 2, 2, 1)
+    reversetr(List(), List(1, 2, 2, 2, 3)) shouldBe List(3, 2, 2, 2, 1)
 
     /**
       * (*) doing the thing *after* the recursive call
       * (*') doing the thing *before* the recursive call
       * (**): In the last call to reverstr, the argument equals the return value
       */
-
-
   }
+
+  "reverse iterative version" in {
+    def reversefoldleft[A](list: List[A]) = {
+      list.foldLeft(List[A]()) {
+        (acc, curr) => curr :: acc
+      }
+    }
+
+    reversefoldleft(List(1, 2, 2, 2, 3)) shouldBe List(3, 2, 2, 2, 1)
+
+    }
 }

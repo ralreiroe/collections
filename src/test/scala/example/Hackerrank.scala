ae1dff62b9b99e6161ce40abb3b2e60996237384
Ralf Oenning <ralf@invicto.net>
2019-11-02 23:31:07 +0000

    cesar half

--- src/test/scala/example/Hackerrank.scala
+++ src/test/scala/example/Hackerrank.scala
@@ -6,34 +6,26 @@ class Hackerrank extends Spec {
   "sfjksjh" in {
 
 
-    /**
-      * 1am => 1, 11am => 11,
-      * 12pm => 12
-      * 1pm => 13, ..., 11pm => 23
-      * 12am => 0
-      */
-    def convertHour(h: Int, isA: Boolean) = {
 
-      (if (h == 12 && isA) 0 else if (isA) h else h + 12).toString
-    }
-
-    println(convertHour(1, true))
-    println(convertHour(1, false))
-    println(convertHour(12, true))
-    println(convertHour(12, false))
+    def cesar(s: String, r: Int =3) = {
 
 
+      val alphabet = "abcdefg"
+      val alphabetr = alphabet.substring(r,alphabet.length)+alphabet.substring(0,r)
 
-    def convertString(s: String) = {
+      println(alphabet)
+      println(alphabetr)
+      println(alphabet,alphabetr)
 
-      val isA = s.substring(s.length-2,s.length).toUpperCase=="AM"
-      convertHour(s.substring(0,2).toInt, isA)+s.substring(2,s.length-2)
+
+      println(alphabet.indexOf('d'))
+
     }
 
 
-    println(convertString("01:05:34am"))
-    println(convertString("01:05:34pm"))
+    cesar("")
   }
+
 
 
 }

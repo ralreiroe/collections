74e6ef47afa08e22df122213252ebdf77bb2cb59
Ralf Oenning <ralf@invicto.net>
2019-11-02 17:55:33 +0000

    staircase string * i

--- src/test/scala/example/Hackerrank.scala
+++ src/test/scala/example/Hackerrank.scala
@@ -8,25 +8,10 @@ class Hackerrank extends Spec {
     def staircase(n: Int) = {
 
       for (i <- 1 to n) {
-        println(createString(" ", n-i) + createString("#", i))
+        println(" " * (n-i) + "#" * i)
       }
 
     }
-
-    def createString(p: String, n: Int) = {
-
-      val seq = for (_ <- 1 to n) yield p
-      seq.foldLeft("") {
-        (acc, curr) => acc+curr
-      }
-    }
-
-    println
-    println(createString(" ", 5))
-    println(createString("#", 1))
-    println
-    staircase(6)
-
 
   }
 

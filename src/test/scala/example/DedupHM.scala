8dbc45919e97e628be32a3799c23dc02969cee89
Ralf Oenning <ralf@invicto.net>
2019-12-22 00:44:37 -0300

    dedup hashset preserve order - build up second list!!

--- src/test/scala/example/DedupHM.scala
+++ src/test/scala/example/DedupHM.scala
@@ -5,15 +5,24 @@ import scala.collection.mutable
 class DedupHM extends Spec {
 
 
-  "" in {
+  "dedup preserving order" in {
 
     val list = List(1,2,3,3,1,5,6,7,3)
 
-
     val set = new mutable.HashSet[Int]()
 
-    list.map(set.add(_))
+    val reslist = mutable.ListBuffer.empty[Int]
 
-    println(set.toList)
+    for (el <- list) {
+
+      if (!set.contains(el)) {
+        set.+=(el)
+        reslist.+=(el)
+      }
+
+    }
+
+    reslist shouldBe mutable.ListBuffer(1, 2, 3, 5, 6, 7)
+
   }
 }

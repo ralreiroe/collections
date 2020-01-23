4b37dd5fee1d553c2196e698392c5ed32ea29a89
Ralf Oenning <ralf@invicto.net>
2020-01-23 15:44:33 +0000

    2nd smallest I

---
+++ src/test/scala/example/SecondSmallestElement.scala
@@ -0,0 +1,22 @@
+package example
+
+class SecondSmallestElement extends Spec {
+
+  "" in {
+
+    val coll = List(2,3,4,7,5,1)
+
+    var min1 = Integer.MIN_VALUE
+    var min2 = Integer.MIN_VALUE
+
+    for (el <- coll) {
+
+      if (el < min1) {
+        min1 =
+      }
+    }
+
+
+  }
+
+}

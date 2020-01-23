9b4be509d01c2e1218a4a4a94444624f1f6d1db2
Ralf Oenning <ralf@invicto.net>
2020-01-23 16:37:53 +0000

    2nd smallest IV

--- src/test/scala/example/SecondSmallestElement.scala
+++ src/test/scala/example/SecondSmallestElement.scala
@@ -2,24 +2,25 @@ package example
 
 class SecondSmallestElement extends Spec {
 
-  "" in {
+  "its a fold!!! G Research Arrgh" in {
 
     val coll = List(2,3,4,7,5,1)
 
     def sndSmallest(coll: List[Int]) = {
-      var min1 = Integer.MAX_VALUE
-      var min2 = Integer.MAX_VALUE
 
-      for (el <- coll) {
+      val twos: (Int, Int) = (Integer.MAX_VALUE, Integer.MAX_VALUE)
 
-        el match {
-          case x if (x < min1) => { min2 = min1; min1 = x }
-          case x if (x < min2) => min2 = x
-          case _ =>
+      coll.foldLeft(twos) {
+        case ((min1, min2), el) => {
+          el match {
+            case x if (x < min1) => (x, min1)
+            case x if (x < min2) => (min1, x)
+            case _ => (min1, min2)
+          }
+
         }
       }
 
-      (min1, min2)
     }
 
     sndSmallest(coll) shouldBe (1,2)

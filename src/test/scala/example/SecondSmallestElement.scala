d6f4a0138efebff22c0ca559898a19d58150f22e
Ralf Oenning <ralf@invicto.net>
2020-01-23 16:23:35 +0000

    2nd smallest IV

--- src/test/scala/example/SecondSmallestElement.scala
+++ src/test/scala/example/SecondSmallestElement.scala
@@ -6,17 +6,23 @@ class SecondSmallestElement extends Spec {
 
     val coll = List(2,3,4,7,5,1)
 
-    var min1 = Integer.MAX_VALUE
-    var min2 = Integer.MAX_VALUE
+    def sndSmallest(coll: List[Int]) = {
+      var min1 = Integer.MAX_VALUE
+      var min2 = Integer.MAX_VALUE
 
-    for (el <- coll) {
+      for (el <- coll) {
 
-      el match {
-        case x if (x<min1) => min1=x
-        case x if (x<min2) => min2=x
-        case _ =>
+        el match {
+          case x if (x < min1) => min1 = x
+          case x if (x < min2) => min2 = x
+          case _ =>
+        }
       }
+
+      min2
     }
+
+    println(sndSmallest(coll))
 
 
   }
